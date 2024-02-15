d58766516a338a0ac9193386db7e3802428d26ec
bentomsett <ben@tomsett.xyz>
2024-02-15 16:18:33 +0000

    Add resizable panes for code editor

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/app/navbar/navbar";
+import {cn} from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="bg-gray-50">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="h-full">
+      <body className={cn(inter.className, "flex flex-col h-full")}>
+        <Navbar />
+        <main className="flex-1 bg-gray-100 p-4">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,41 @@
-import Navbar from "@/components/app/navbar/navbar";
+"use client";
+
+import {useLayoutEffect, useState} from "react";
+import {ResizableHandle, ResizablePanel, ResizablePanelGroup} from "@/components/ui/resizable";
+
+const useWindowSize = () => {
+  const [size, setSize] = useState([0, 0]);
+
+  useLayoutEffect(() => {
+    const updateSize = () => {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+
+    window.addEventListener('resize', updateSize);
+    updateSize();
+
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+
+  return size;
+};
 
 export default function Home() {
+  const [width] = useWindowSize();
+
   return (
-    <Navbar />
+    <ResizablePanelGroup direction={width > 992 ? 'horizontal' : 'vertical'}>
+      <ResizablePanel defaultSize={70}>
+        <div className="bg-white rounded-md p-4 h-full">
+          Code editor here
+        </div>
+      </ResizablePanel>
+      <ResizableHandle />
+      <ResizablePanel defaultSize={30} minSize={20}>
+        <div className="bg-white rounded-md p-4 h-full">
+          Registers here
+        </div>
+      </ResizablePanel>
+    </ResizablePanelGroup>
   );
 }
